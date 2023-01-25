3375f13808e8c4ba2203f283b018984966d2e987
Jordan Eleniak <jordaneleniak@gmail.com>
2023-01-25 00:19:55 -0700

    Started altering logic to include functions to clean up the code and eliminate redundant commands. Added a getValidCode() function, which will get a valid code from the user. Also added a validateCode() function which will make sure that the entered code is a 4 digit (int) code. Put the current code in a main() function.

--- mastermind.py
+++ mastermind.py
@@ -20,35 +20,71 @@ clear = lambda: os.system('cls') # This function will clear the screen upon a su
 ## Declaring boolean test variables to make sure Player One has entered a valid code
 #   and verifies that they want to use the code entered.
 #
-successfulCodeEntered = False
-correctCode = False
-while successfulCodeEntered == False:  # This code will keep looping until a valid and verified code is entered.
-    try:
-        playerOne = int(input("Please enter a 4 digit number: "))
-        while correctCode == False:
-            if len(str(playerOne)) == 4:
-                print("You have entered: " + str(playerOne) + ".")
-                isCodeCorrect = input("Is this the code you want? Yes/No: ").lower()
-                if isCodeCorrect == 'y' or isCodeCorrect == 'yes':
-                    correctCode = True
-                    successfulCodeEntered = True
-                    clear()
-                elif isCodeCorrect == 'n' or isCodeCorrect == 'no':
-                    playerOne = int(input("Please enter a new 4 digit number: "))
+
+def main():
+    successfulCodeEntered = False
+    correctCode = False
+    while successfulCodeEntered == False:  # This code will keep looping until a valid and verified code is entered.
+        try:
+            playerOne = int(input("Please enter a 4 digit number: "))
+            while correctCode == False:
+                if len(str(playerOne)) == 4:
+                    print("You have entered: " + str(playerOne) + ".")
+                    isCodeCorrect = input("Is this the code you want? Yes/No: ").lower()
+                    if isCodeCorrect == 'y' or isCodeCorrect == 'yes':
+                        correctCode = True
+                        successfulCodeEntered = True
+                        clear()
+                    elif isCodeCorrect == 'n' or isCodeCorrect == 'no':
+                        playerOne = int(input("Please enter a new 4 digit number: "))
+                    else:
+                        print("Please enter yes or no.")
+                elif len(str(playerOne)) < 4:
+                    print("Code length not long enough. Please try again.")
+                    playerOne = int(input("Please enter a 4 digit number: "))
+                elif len(str(playerOne)) > 4:
+                    print("Code length too long enough. Please try again.")
+                    playerOne = int(input("Please enter a 4 digit number: "))
                 else:
-                    print("Please enter yes or no.")
-            elif len(str(playerOne)) < 4:
-                print("Code length not long enough. Please try again.")
-                playerOne = int(input("Please enter a 4 digit number: "))
-            elif len(str(playerOne)) > 4:
-                print("Code length too long enough. Please try again.")
-                playerOne = int(input("Please enter a 4 digit number: "))
-            else:
-                print("Error: How did you get here?")
+                    print("Error: How did you get here?")
+        except:
+            print("Error: You did not enter a valid code. Please enter numbers only!")
+
+    ## Player Two
+    #
+    # This is where Player Two tries and guess the code that Player One has picked.
+    #
+
+## Get Valid Code
+#
+# This function gets a valid code.
+#
+def getValidCode():
+    try:
+        validCode = False
+        playerOne = int(input("Please enter a 4 digit number: "))
+        while validCode == False:
+            validCode = validateCode(playerOne)
+            if validCode == False:
+                playerOne = int(input("Please enter a new 4 digit number: "))
     except:
         print("Error: You did not enter a valid code. Please enter numbers only!")
 
-## Player Two
+
+## Validate Code
 #
-# This is where Player Two tries and guess the code that Player One has picked.
+# This function validates the entered code.
 #
+def validateCode(playerOne):
+    validCode = False
+    if len(str(playerOne)) < 4:
+        print("Sorry, the code you entered is too short. Please try again.")
+    elif len(str(playerOne)) > 4:
+        print("Sorry, the code you entered is too long. Please try again.")
+    else:
+        validCode = True
+    return validCode
+
+## Start the program
+#
+main()
