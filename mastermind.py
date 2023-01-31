4f625a9c085e2cd503a9cd1d3885447d9d6d8ba3
Jordan Eleniak <jordaneleniak@gmail.com>
2023-01-31 14:30:29 -0700

    Deleted the old code from version 0.1 and now successfully replaced with functions.

--- mastermind.py
+++ mastermind.py
@@ -20,36 +20,12 @@ clear = lambda: os.system('cls') # This function will clear the screen upon a su
 ## Declaring boolean test variables to make sure Player One has entered a valid code
 #   and verifies that they want to use the code entered.
 #
-'''
+
 def main():
-    successfulCodeEntered = False
-    correctCode = False
-    while successfulCodeEntered == False:  # This code will keep looping until a valid and verified code is entered.
-        try:
-            playerOne = int(input("Please enter a 4 digit number: "))
-            while correctCode == False:
-                if len(str(playerOne)) == 4:
-                    print("You have entered: " + str(playerOne) + ".")
-                    isCodeCorrect = input("Is this the code you want? Yes/No: ").lower()
-                    if isCodeCorrect == 'y' or isCodeCorrect == 'yes':
-                        correctCode = True
-                        successfulCodeEntered = True
-                        clear()
-                    elif isCodeCorrect == 'n' or isCodeCorrect == 'no':
-                        playerOne = int(input("Please enter a new 4 digit number: "))
-                    else:
-                        print("Please enter yes or no.")
-                elif len(str(playerOne)) < 4:
-                    print("Code length not long enough. Please try again.")
-                    playerOne = int(input("Please enter a 4 digit number: "))
-                elif len(str(playerOne)) > 4:
-                    print("Code length too long enough. Please try again.")
-                    playerOne = int(input("Please enter a 4 digit number: "))
-                else:
-                    print("Error: How did you get here?")
-        except:
-            print("Error: You did not enter a valid code. Please enter numbers only!")
-'''
+    print("Welcome to MASTERMIND!")
+    print("The master code breaking game.")
+    getValidCode()
+
     ## Player Two
     #
     # This is where Player Two tries and guess the code that Player One has picked.
@@ -90,8 +66,6 @@ def validateCode(codeToTest):
 #
 # This function verifies with the user that the code entered is the code
 #   that they want to use.
-# TODO Verify the logic and check that the call to getValidCode() doesn't
-#   get stuck in an infinite loop.
 #
 def verifyCode(code):
     codeVerified = False
@@ -109,5 +83,4 @@ def verifyCode(code):
 
 ## Start the program
 #
-#main()
-#getValidCode()
+main()
